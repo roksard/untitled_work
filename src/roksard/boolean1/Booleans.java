0663a0f41e852003a2414a483adeebff432eac30
roksard <roksard@gmail.com>
2019-12-13 10:12:52 +0500

    Objects.equals Boolean vs boolean vs null

--- src/roksard/boolean1/Booleans.java
+++ src/roksard/boolean1/Booleans.java
@@ -1,7 +1,9 @@
 package roksard.boolean1;
 
+import java.util.Objects;
+
 public class Booleans {
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
 //        System.out.println(Boolean.TRUE.equals(null));
 //        System.out.println(Boolean.FALSE.equals(null));
         Boolean xoxo = null;
@@ -10,5 +12,13 @@ public class Booleans {
         if(xoxo) {
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Boolean _true = Boolean.TRUE;
+        Boolean _null = null;
+        System.out.println(Objects.equals(_true, true));
+        System.out.println(Objects.equals(_null, true));
+        System.out.println(Objects.equals(_null, _true));
     }
 }
