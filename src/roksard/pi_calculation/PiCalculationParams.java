cf853c01c84c12d93b4d147dbcfb32d27e8819ac
roksard <roksard@gmail.com>
2020-02-07 16:35:45 +0500

    roksard\pi_calculation

---
+++ src/roksard/pi_calculation/PiCalculationParams.java
@@ -0,0 +1,7 @@
+package roksard.pi_calculation;
+
+public class PiCalculationParams {
+    public static final long iterations = 20000000000L;
+    public static final int divisionRounding = 8;
+
+}
