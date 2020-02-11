c24ab9113f631676a6bd225d6586e042fbe1a5a0
roksard <roksard@gmail.com>
2020-02-11 15:29:44 +0500

    roksard\concurrency\DateFormatNonSafe +print

--- src/roksard/concurrency/DateFormatNonSafe.java
+++ src/roksard/concurrency/DateFormatNonSafe.java
@@ -9,12 +9,12 @@ import java.util.concurrent.Executors;
 public class DateFormatNonSafe {
     public static void main(String[] args) {
         final SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
-        ExecutorService ex = Executors.newFixedThreadPool(1000);
+        ExecutorService ex = Executors.newFixedThreadPool(5);
         for (;;) {
             ex.execute(new Runnable() {
                 public void run() {
                     try {
-                        f.format(new Date(new Random().nextLong()));
+                        System.out.print(f.format(new Date(new Random().nextLong())) + ", ");
                     } catch (Exception e) {
                         e.printStackTrace();
                         System.exit(1);
