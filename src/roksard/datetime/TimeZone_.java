4da72cdc7948d6e26bd1f77f9e15d58fa6a18a9e
roksard <roksard@gmail.com>
2020-02-12 11:33:44 +0500

    roksard\datetime\TimeZone_

--- src/roksard/datetime/TimeZone_.java
+++ src/roksard/datetime/TimeZone_.java
@@ -1,19 +1,22 @@
 package roksard.datetime;
 
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.TimeZone;
 
 public class TimeZone_ {
     public static void main(String[] args) {
-        Instant inst = Instant.now();
-        ZonedDateTime zonedDateTime = inst.atZone(ZoneId.of("Asia/Yekaterinburg"));
-        Timestamp.valueOf(ZonedDateTime.now(ZoneId.of("Asia/Yekaterinburg")).toLocalDateTime());
         System.out.println(Arrays.toString(TimeZone.getAvailableIDs()));
+//        Clock defaultClock = Clock.system(ZoneId.of("GMT+1"));
+//        Instant inst = Instant.now(defaultClock);
+//        Timestamp t1 = new Timestamp(inst.toEpochMilli());
+        Timestamp t2 = Timestamp.valueOf(ZonedDateTime.now(ZoneId.of("Asia/Yekaterinburg")).toLocalDateTime());
+        //System.out.println(t1);
+        System.out.println(t2);
     }
 }
