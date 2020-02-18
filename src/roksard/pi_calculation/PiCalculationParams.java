c64d6b0354cf7478ca4e8b80da31fc61838ef325
roksard <roksard@gmail.com>
2020-02-18 11:18:14 +0500

    add Maven support

--- src/roksard/pi_calculation/PiCalculationParams.java
+++
@@ -1,7 +0,0 @@
-package roksard.pi_calculation;
-
-public class PiCalculationParams {
-    public static final long iterations = 20000000000L;
-    public static final int divisionRounding = 8;
-
-}
--- src/roksard/stringjoiner/Util.java
+++
@@ -1,7 +0,0 @@
-package roksard.stringjoiner;
-
-public class Util {
-    public static boolean isEmpty(String string) {
-        return string == null || string.isEmpty();
-    }
-}
