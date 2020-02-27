77a0692863661c4c746fe6f19e5b5c959eb3fe94
roksard <roksard@gmail.com>
2020-02-27 22:01:14 +0500

    html parsing - initial

---
+++ src/main/java/roksard/html_parsing/NodeCriteria.java
@@ -0,0 +1,35 @@
+package roksard.html_parsing;
+
+public class NodeCriteria {
+    String className;
+    String tag;
+
+    public boolean tagFits(String value) {
+        return value.matches(tag);
+    }
+
+    public boolean classNameFits(String value) {
+        return value.matches(className);
+    }
+
+    public NodeCriteria(String className, String tag) {
+        this.className = className;
+        this.tag = tag;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
