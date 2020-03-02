9a05f07725f449b6da36b93de2f023c0f175ab3c
roksard <roksard@gmail.com>
2020-03-02 16:13:57 +0500

    roksard\collections\Map

--- src/main/java/roksard/collections/Map/MapCompute.java
+++ src/main/java/roksard/collections/Map/MapCompute.java
@@ -3,14 +3,19 @@ package roksard.collections.Map;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.Random;
 
 public class MapCompute {
     public static void main(String[] args) {
-        Map<Integer, BigDecimal> map = new HashMap<>();
+        Map<Boolean, BigDecimal> map = new HashMap<>();
         //map.put(1, BigDecimal.TEN);
-        BigDecimal value = BigDecimal.valueOf(15);
-        map.compute(1, (Integer key, BigDecimal current) -> current == null ? value : value.add(current));
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            boolean flag = rand.nextBoolean();
+            BigDecimal value = BigDecimal.valueOf(i);
+            System.out.println(flag + ": " + i);
+            map.compute(flag, (Boolean key, BigDecimal current) -> current == null ? value : current.add(value));
+        }
         System.out.println(map);
     }
 }
