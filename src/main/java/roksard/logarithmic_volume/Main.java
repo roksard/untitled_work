4222b91b9b72686cb3d5a1a2e8c5a20834865269
ruf <ruf@proitr.ru>
2020-03-06 08:50:35 +0500

    src\main\java\roksard\logarithmic_volume remove unnecessary code

--- src/main/java/roksard/logarithmic_volume/Main.java
+++ src/main/java/roksard/logarithmic_volume/Main.java
@@ -1,8 +1,5 @@
 package roksard.logarithmic_volume;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     static void printVol(int vol, double lvol) {
         if (vol % 10 != 0) {
@@ -16,7 +13,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        List<Double> volList = new ArrayList<>();
         for(int vol = 0; vol <= 100; vol++) {
             double lvol = Math.pow(vol, 2) / 100;
             printVol(vol, lvol);
