a2fe7a044a83becdf68b2920e7d5f554a6f2ee5d
ruf <ruf@proitr.ru>
2020-03-06 08:50:03 +0500

    src\main\java\roksard\logarithmic_volume

--- src/main/java/roksard/logarithmic_volume/Main.java
+++ src/main/java/roksard/logarithmic_volume/Main.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    static void printVol(int vol, double lvol) {
+        if (vol % 10 != 0) {
+            return;
+        }
+        System.out.format("%3d = %6.2f%% ", vol, lvol);
+        for (int i = 0; i < lvol; i++) {
+            System.out.print(".");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         List<Double> volList = new ArrayList<>();
         for(int vol = 0; vol <= 100; vol++) {
             double lvol = Math.pow(vol, 2) / 100;
-            System.out.format("%.2f%% ", lvol);
-            for(int i = 0; i < lvol; i++) {
-                System.out.print(".");
-            }
-            System.out.println();
+            printVol(vol, lvol);
         }
     }
 }
