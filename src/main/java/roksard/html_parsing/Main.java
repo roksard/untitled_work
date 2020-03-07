06eb941798ac94ace06b2b02aee92225aac4f796
roksard <roksard@gmail.com>
2020-03-07 20:34:36 +0500

    roksard\html_parsing remove imports

--- src/main/java/roksard/html_parsing/Main.java
+++ src/main/java/roksard/html_parsing/Main.java
@@ -4,7 +4,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.TextNode;
-import org.jsoup.select.Elements;
 import roksard.html_parsing.NodeFinder.NodeCriteria;
 
 import java.io.File;
