d009433cf97d0472a7f07c3428fc184c71995865
roksard <roksard@gmail.com>
2020-03-09 16:51:13 +0500

    roksard\datetime\ZoneId_.java

---
+++ src/main/java/roksard/datetime/ZoneId_.java
@@ -0,0 +1,10 @@
+package roksard.datetime;
+
+import java.time.ZoneId;
+
+public class ZoneId_ {
+    public static void main(String[] args) {
+        System.out.println("System timezone: ");
+        System.out.println(ZoneId.systemDefault());
+    }
+}
