0af5fe961f40a01c7b5b98f216ccbce5996c85a2
roksard <roksard@gmail.com>
2020-03-30 09:16:47 +0500

    roksard\geometry\getPointOnVector\GetPointOnVector fix sign / direction

--- src/main/java/roksard/geometry/getPointOnVector/GetPointOnVector.java
+++ src/main/java/roksard/geometry/getPointOnVector/GetPointOnVector.java
@@ -3,8 +3,12 @@ package roksard.geometry.getPointOnVector;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GetPointOnVector {
+    static Random rand = new Random();
     public static Point getPointOnVector(Point vectorBegin, Point vectorEnd, double desiredDistFromVectorBegin) {
         double x1 = vectorBegin.getX();
         double y1 = vectorBegin.getY();
@@ -12,21 +16,18 @@ public class GetPointOnVector {
         double y2 = vectorEnd.getY();
         double deltaY = y2-y1;
         double deltaX = x2-x1;
+        double signX = deltaX < 0 ? -1 : 1;
+        double signY = deltaY < 0 ? -1 : 1;
         double alpha = Math.atan(deltaY / deltaX);
-        double newX = x1 + desiredDistFromVectorBegin * Math.sin(Math.PI/2 - alpha);
-        double newY = y1 + desiredDistFromVectorBegin * Math.sin(alpha);
+        double newX = x1 + desiredDistFromVectorBegin * Math.abs(Math.sin(Math.PI/2 - alpha)) * signX;
+        double newY = y1 + desiredDistFromVectorBegin * Math.abs(Math.sin(alpha))  * signY;
         return new Point((int)Math.round(newX), (int)Math.round(newY));
     }
 
     public static void main(String[] args) throws InterruptedException {
-        int x1 = 50;
-        int y1 = 50;
-        int x2 = 200;
-        int y2 = 70;
-        Point np = getPointOnVector(new Point(x1, y1), new Point(x2, y2), 2);
-        Point np2 = getPointOnVector(new Point(x1, y1), new Point(x2, y2), 50);
-        Point np3 = getPointOnVector(new Point(x1, y1), new Point(x2, y2), -40);
-        Point np4 = getPointOnVector(new Point(x1, y1), new Point(x2, y2), 200);
+        Point begin = new Point(294, 237);
+        Point end = new Point(177, 198);
+        Point np = getPointOnVector(begin, end, begin.distance(end) / 2);
 
         JFrame frame = new JFrame();
         frame.add(new JPanel() {
@@ -46,12 +47,9 @@ public class GetPointOnVector {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                drawCircle(g, x1, y1, 2, Color.CYAN);
-                drawCircle(g, x2, y2, 2, Color.CYAN);
-                drawCircle(g, np.getX(), np.getY(), 4, Color.CYAN);
-                drawCircle(g, np2.getX(), np2.getY(), 4, Color.RED);
-                drawCircle(g, np3.getX(), np3.getY(), 4, Color.YELLOW);
-                drawCircle(g, np4.getX(), np4.getY(), 4, Color.GREEN);
+                drawCircle(g, begin.getX(), begin.getY(), 4, Color.GREEN);
+                drawCircle(g, end.getX(), end.getY(), 4, Color.RED);
+                drawCircle(g, np.getX(), np.getY(), 4, Color.BLUE);
             }
         });
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -59,5 +57,14 @@ public class GetPointOnVector {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.repaint();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                begin.setLocation(rand.nextInt(300)+10, rand.nextInt(300)+10);
+                end.setLocation(rand.nextInt(300)+10, rand.nextInt(300)+10);
+                np.setLocation(getPointOnVector(begin, end, begin.distance(end) *  9/10));
+                frame.repaint();
+            }
+        }, 2000, 2000);
     }
 }
