85f3ba57e1fe99eb30ea85bb6120a014b3d5c99c
roksard <roksard@gmail.com>
2020-06-25 12:51:29 +0500

    roksard\util\Logger add log to file functionality

--- src/main/java/roksard/util/Logger.java
+++ src/main/java/roksard/util/Logger.java
@@ -1,5 +1,6 @@
 package roksard.util;
 
+import java.io.*;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -9,13 +10,23 @@ import java.util.Arrays;
 public class Logger {
     private String prefix;
     private boolean isDisabled = false;
+    private String fileName;
 
     public static Logger getLogger(String prefix) {
         return new Logger(prefix);
     }
 
+    public static Logger getLogger(String fileName, String prefix) {
+        return new Logger(fileName, prefix);
+    }
+
     private Logger(String prefix) {
         this.prefix = prefix;
+    }
+
+    private Logger(String fileName, String prefix) {
+        this.prefix = prefix;
+        this.fileName = fileName;
     }
 
     public static String timestamp() {
@@ -29,10 +40,37 @@ public class Logger {
     }
 
     public void log(Object... toPrint) {
-        if (!isDisabled) {
-            System.out.println(timestamp() + " --" + prefix + "-- " + Arrays.stream(toPrint)
-                    .map(Object::toString)
-                    .reduce("", (a, b) -> a + " " + b));
+        FileOutputStream fs = null;
+        PrintStream ps = null;
+        PrintStream printStream = System.out;
+        try {
+            if (fileName != null) {
+                try {
+                    fs = new FileOutputStream(fileName, false);
+                    ps = new PrintStream(fs);
+                    printStream = ps;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new RuntimeException("Could not create print stream!");
+                }
+            }
+            if (!isDisabled) {
+                printStream.println(timestamp() + " --" + prefix + "-- " + Arrays.stream(toPrint)
+                        .map(Object::toString)
+                        .reduce("", (a, b) -> a + " " + b));
+            }
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (fs != null) {
+                try {
+                    fs.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
+
 }
