c815a29b58f22b4fc60c7be4c99a36d0c47e0b8e
roksard <roksard@gmail.com>
2020-06-25 11:07:06 +0500

    roksard\byte_conversion_unsigned  using Format. util method

--- src/main/java/roksard/byte_conversion_unsigned/ByteConversion.java
+++ src/main/java/roksard/byte_conversion_unsigned/ByteConversion.java
@@ -1,11 +1,13 @@
 package roksard.byte_conversion_unsigned;
 
+import roksard.util.Format;
+
 public class ByteConversion {
     public static void main(String[] args) {
         byte b = Byte.MIN_VALUE;
         while (true) {
             int unsigned = Byte.toUnsignedInt(b);
-            String hex = String.format("%2s", Integer.toHexString(unsigned)).replace(" ", "0");
+            String hex = Format.byteAsHex(b);
             System.out.printf("%d = %d = %s \n", b, unsigned, "0x"+hex);
             if (b < Byte.MAX_VALUE) {
                 b++;
