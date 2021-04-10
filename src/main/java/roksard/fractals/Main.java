d8fdb9e07902231721feea065ad5282b8c04327a
roksard <roksard@gmail.com>
2021-04-10 19:01:19 +0200

    fractals fix imports

--- src/main/java/roksard/fractals/Main.java
+++ src/main/java/roksard/fractals/Main.java
@@ -1,11 +1,6 @@
 package roksard.fractals;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
