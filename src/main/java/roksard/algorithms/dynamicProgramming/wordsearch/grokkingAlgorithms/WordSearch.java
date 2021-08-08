252ec36d7b7fc67db2ac1edc4113bda509fef9e3
roksard <roksard@gmail.com>
2021-08-08 21:31:54 +0200

    algorithms/wordsearch/grokking searchng subsequences only at same pos withing a word

--- src/main/java/roksard/algorithms/dynamicProgramming/wordsearch/grokkingAlgorithms/WordSearch.java
+++ src/main/java/roksard/algorithms/dynamicProgramming/wordsearch/grokkingAlgorithms/WordSearch.java
@@ -2,7 +2,7 @@ package roksard.algorithms.dynamicProgramming.wordsearch.grokkingAlgorithms;
 
 public class WordSearch {
     public static void main(String[] args) {
-        System.out.println("seq: " + new WordSearch().getCommonSequence("abcabc", "abcdef", true));
+        System.out.println("seq: " + new WordSearch().getCommonSequence("funken", "fucken", true));
     }
 
     public int getCommonSequence(String wordA, String wordB, boolean debugPrint) {
@@ -11,24 +11,19 @@ public class WordSearch {
         int totalSequence = 0;
         for (int i = 0; i < size; i++) {
             boolean sequenceContinued = false;
-            int currentMax = 0;
-            for (int n = 0; n < size; n++) {
-                int currentSequence = 0;
-                if (i > 0 && n > 0) {
-                    currentSequence = data[i-1][n-1];
-                }
-                boolean charsEqual = wordA.charAt(i) == wordB.charAt(n);
-                if (charsEqual) {
-                    sequenceContinued = currentSequence > 0;
-                    currentSequence += 1;
-                    currentMax = Math.max(currentMax, currentSequence);
-                }
-                data[i][n] = charsEqual ? currentSequence : 0;
+            int currentSequence = 0;
+            if (i > 0) {
+                currentSequence = data[i-1][i-1];
             }
+            boolean charsEqual = wordA.charAt(i) == wordB.charAt(i);
+            if (charsEqual) {
+                sequenceContinued = currentSequence > 0;
+                currentSequence += 1;
+            }
+            data[i][i] = charsEqual ? currentSequence : 0;
             //sequence broke, adding its size to totalSequence
-            if (!sequenceContinued || i == size-1) {
-                totalSequence += currentMax;
-                currentMax = 0;
+            if (!charsEqual || i == size-1) {
+                totalSequence += currentSequence;
             }
         }
 
