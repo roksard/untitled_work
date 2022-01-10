1d56e0f2ef316297406e778807041bb134e8df34
roksard <roksard@gmail.com>
2022-01-10 21:28:32 +0100

    strings.StringFormat add format int

--- src/main/java/roksard/strings/StringFormat.java
+++ src/main/java/roksard/strings/StringFormat.java
@@ -1,9 +1,10 @@
 package roksard.strings;
 
+import java.sql.SQLOutput;
 import java.util.StringJoiner;
 
 public class StringFormat {
-    public static void main(String[] args) {
+    static void formatStringColumns() {
         String s1 = "";
         String s2 = "abcdef";
         String s3 = s1+s2;
@@ -14,6 +15,16 @@ public class StringFormat {
         System.out.println(String.format(format, s1, s2, s3));
         System.out.println(String.format(format, s2, s3, s1));
         System.out.println(String.format(format, s3, s1, s2));
+    }
 
+    static void formatInt() {
+        String formatted1 = String.format("%2d", 5);
+        System.out.println("formatted1 = '" + formatted1 + "'");
+
+        String formatted2 = String.format("%02d", 5);
+        System.out.println("formatted2 = '" + formatted2 + "'");
+    }
+    public static void main(String[] args) {
+        formatInt();
     }
 }
