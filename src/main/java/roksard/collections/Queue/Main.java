bae9e073c8ddfdd3fb04f9d53fc4ed5d183e4b08
roksard <roksard@gmail.com>
2022-01-22 21:20:51 +0100

    collections.Deque

--- src/main/java/roksard/collections/Queue/Main.java
+++ src/main/java/roksard/collections/Queue/Main.java
@@ -1,15 +1,22 @@
 package roksard.collections.Queue;
 
-import java.util.Queue;
+import java.util.Deque;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedBlockingDeque<>(5);
+        Deque<Integer> dequeue = new LinkedBlockingDeque<>(5);
 
         for(int i = 0; i < 10; i++) {
-            System.out.println(queue.offer(i));
+            System.out.println(dequeue.offerFirst(i));
+            System.out.println(dequeue);
         }
-        System.out.println(queue);
+
+        System.out.println("--- removing ---");
+
+        while (!dequeue.isEmpty()) {
+            System.out.println(dequeue.pollLast());
+            System.out.println(dequeue);
+        }
     }
 }
